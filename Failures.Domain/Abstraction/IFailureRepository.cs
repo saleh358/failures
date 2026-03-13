9714f8be4bb7ba2dee46c3f39b3bdfd8663a2208
Saleh Salem <saleh.salem@iken.tech>
2026-03-13 16:05:49 +0200

    add repository

---
+++ Failures.Domain/Abstraction/IFailureRepository.cs
@@ -0,0 +1,12 @@
+﻿using Failures.Domain.Entities;
+
+namespace Failures.Domain.Abstraction;
+
+public interface IFailureRepository
+{
+    public Task<FailedPayment> GetFailureByIdAsync(Guid id);
+    public Task<IEnumerable<FailedPayment>> GetAllEntities();
+    public Task AddFailureAsync(FailedPayment failedPayment);
+    public Task DeleteFailureAsync(Guid id);
+    public Task UpdateFailureAsync(FailedPayment failedPayment);
+}
---
+++ Failures.Infrastructure/Persistence/EntityRepository.cs
@@ -0,0 +1,47 @@
+﻿using Failures.Domain.Abstraction;
+using Failures.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Failures.Infrastructure.Persistence;
+
+public class FailureRepository : IFailureRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public FailureRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddFailureAsync(FailedPayment failedPayment)
+    {
+        await _context.FailedPayments.AddAsync(failedPayment);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteFailureAsync(Guid id)
+    {
+        var failure = await _context.FailedPayments.FindAsync(id);
+        if (failure != null)
+        {
+            _context.FailedPayments.Remove(failure);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<IEnumerable<FailedPayment>> GetAllEntities()
+    {
+        return await _context.FailedPayments.ToListAsync();
+    }
+
+    public async Task<FailedPayment> GetFailureByIdAsync(Guid id)
+    {
+        return await _context.FailedPayments.FindAsync(id);
+    }
+
+    public async Task UpdateFailureAsync(FailedPayment failedPayment)
+    {
+        _context.FailedPayments.Update(failedPayment);
+        await _context.SaveChangesAsync();
+    }
+}
